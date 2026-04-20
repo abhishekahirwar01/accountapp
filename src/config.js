b3146229c836ce0b5c74ba78b6d4770f79b1de5d
Abhishek Ahirwar <ahirwarabhi01@gmail.com>
2026-04-20 15:04:52 +0530

    resolve errors

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
-// export const BASE_URL =
-//   'https://accountapp-backend-shardaassociates.onrender.com';
+export const BASE_URL =
+  'https://accountapp-backend-shardaassociates.onrender.com';
 // export const BASE_URL =
 //   'https://accountapp-backend-ooxj.vercel.app';
 
 // export const BASE_URL = 'https://vinimaynativebe.sharda.co.in';
 
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 
-export const BASE_URL =
-  Platform.OS === 'android'
-    ? 'http://10.0.2.2:8777' // emulator
-    : 'http://localhost:8777';
+// export const BASE_URL =
+//   Platform.OS === 'android'
+//     ? 'http://10.0.2.2:8777' // emulator
+//     : 'http://localhost:8777';
